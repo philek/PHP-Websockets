0e2cd5d786590f2319c4c73e544b01ab05662bee
Filip Rutkowski <philek+github@gmail.com>
2016-02-14 21:54:17 +0100

    Made tiny ehco server.

--- server.php
+++ server.php
@@ -1,12 +1,27 @@
 #!/usr/bin/env php
 <?php
-namespace \Phpws;
 
-require_once(__DIR__ . '/core/bootstrap.php');
+namespace Phpws;
 
-class WebsocketServer extends \Phpws\Core\Server\Server {
-    use \Phpws\Core\Eventloop\Socket;
+require_once(__DIR__ . '/src/bootstrap.php');
+
+class MyApplication extends \Gpws\Core\Application {
+	public function onMessage(\Gpws\Interfaces\Client $client, string $message) {
+		$x = new \Gpws\Message\TextMessage('ECHOOO ' . $message);
+
+		$client->queueMessage($x);
+
+	}
 }
 
-$ws = new WebsocketServer('0.0.0.0', 9000);
+
+$myApp = new MyApplication();
+
+
+$ws = new \Gpws\Core\Server();
+
+$ws->bind('0.0.0.0', 6100);
+
+$ws->registerApp('/', $myApp);
+
 $ws->run();
